d0b09a6047e8c33bb8ede37cc787132eb6417e0e
quanet <quanet@163.com>
2014-05-13 21:23:32 +0800

    修复Maven运行的缺陷

---
+++ sessionmgt-server/src/main/java/com/quanix/app/integration/server/shiro/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.quanix.app.integration.server.shiro.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : lihaoquan
+ */
+@Controller("/test")
+public class IndexController {
+
+    @RequestMapping(value = "/index")
+    public String goHome() {
+
+        System.out.println("======= go home now ");
+
+        return "index.jsp";
+    }
+}
